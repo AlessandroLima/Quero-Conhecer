4437d16ba8416884b4547a94bee8f03d52cd1431
Alessandro <alessandro@iMac-de-Alessandro.local>
2019-08-31 17:06:35 -0300

    Inicial

--- Views/MapViewViewController.swift
+++ Views/MapViewViewController.swift
@@ -7,16 +7,50 @@
 //
 
 import UIKit
+import MapKit
+
 
 class MapViewViewController: UIViewController {
 
+    //MARK: Outlets
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var MapView: MKMapView!
+    @IBOutlet weak var lbName: UILabel!
+    @IBOutlet weak var lbAddress: UILabel!
+    @IBOutlet weak var viInfo: UIView!
+    @IBOutlet weak var ctSearchBar: NSLayoutConstraint!
+    
+    //MARK: Properties
+    
+    var constraint:NSLayoutConstraint?
+    
+    //MARK: View Function
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func ShowRote(_ sender: UIButton) {
+    
+    }
+    @IBAction func showSearchBar(_ sender: UIBarButtonItem) {
+        constraint = ctSearchBar
+        self.view.layoutIfNeeded()
+        UIView.animate(withDuration: 1.5) {
+            
+            if self.ctSearchBar.constant == CGFloat(0){
+                self.ctSearchBar.constant = CGFloat(44.0)
+            }else{
+                self.ctSearchBar.constant = CGFloat(0)
+            }
+            self.view.layoutIfNeeded()
+        
+        }
+        
+        
+    }
+    
     /*
     // MARK: - Navigation
 
--- Views/PlaceFinderViewController.swift
+++ Views/PlaceFinderViewController.swift
@@ -7,24 +7,55 @@
 //
 
 import UIKit
+import MapKit
 
 class PlaceFinderViewController: UIViewController {
 
+    //MARK:Outlets
+    @IBOutlet weak var txtSearch: UITextField!
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var viewLoading: UIView!
+    @IBOutlet weak var loading: UIActivityIndicatorView!
+    
+    //MARK:Properties
+    
+    let geoCoder = CLGeocoder()
+    
+    
+    //MARK:View Functions
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func findPlace(_ sender: UIButton) {
+        txtSearch.resignFirstResponder()
+        let place = txtSearch.text
+        load(show: true)
+        if let place = place{
+            geoCoder.geocodeAddressString(place) { (placeMarks, error) in
+                self.load(show: false)
+            }
+        }
+        
+    }
+    
+    @IBAction func close(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
     }
     
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension PlaceFinderViewController{
+    
+    //MARK:Functions
+    func load(show:Bool){
+            viewLoading.isHidden = !show
+        if show{
+            loading.startAnimating()
+        }else{
+            loading.stopAnimating()
+        }
     }
-    */
-
 }
