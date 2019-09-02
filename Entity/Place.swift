18f9ba898295a46f2deed82d712c73e637d1913c
aletlima <aletlima@gmail.com>
2019-09-02 10:46:27 -0300

    Criação do .gitignore e Utils

--- Entity/Place.swift
+++ Entity/Place.swift
@@ -7,3 +7,51 @@
 //
 
 import Foundation
+import MapKit
+
+struct Place {
+    
+    let name:String
+    let latitude:CLLocationDegrees
+    let longitude:CLLocationDegrees
+    let address:String
+    
+    var coordinate: CLLocationCoordinate2D{
+        return CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+    
+//    public init(name:String,latitude:CLLocationDegrees,longitude:CLLocationDegrees,address:String){
+//        self.name = name
+//        self.latitude = latitude
+//        self.longitude = longitude
+//        self.address = address
+//    }
+    
+    static func getFormattedAddress(whith placeMark:CLPlacemark) -> String{
+        var address = ""
+        if let street = placeMark.thoroughfare{
+            address += street
+        }
+        if let number = placeMark.subThoroughfare{
+            address += " \(number)"
+        }
+        if let numsubLocality = placeMark.subLocality{
+            address += " \(numsubLocality)"
+        }
+        if let city = placeMark.locality{
+            address += "\n\(city)"
+        }
+        if let state = placeMark.administrativeArea{
+            address += " - \(state)"
+        }
+        if let postalCode = placeMark.postalCode{
+            address += "\n\(postalCode)"
+        }
+        if let country = placeMark.country{
+            address += " \(country)"
+        }
+        
+        
+        return address
+    }
+}
--- Views/PlaceFinderViewController.swift
+++ Views/PlaceFinderViewController.swift
@@ -10,7 +10,12 @@ import UIKit
 import MapKit
 
 class PlaceFinderViewController: UIViewController {
-
+    
+    enum placeFinderMessageType {
+        case error(String)
+        case confirmation(String)
+    }
+    
     //MARK:Outlets
     @IBOutlet weak var txtSearch: UITextField!
     @IBOutlet weak var mapView: MKMapView!
@@ -20,12 +25,13 @@ class PlaceFinderViewController: UIViewController {
     //MARK:Properties
     
     let geoCoder = CLGeocoder()
+    var place: Place!
     
     
     //MARK:View Functions
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
     }
     
     @IBAction func findPlace(_ sender: UIButton) {
@@ -35,27 +41,70 @@ class PlaceFinderViewController: UIViewController {
         if let place = place{
             geoCoder.geocodeAddressString(place) { (placeMarks, error) in
                 self.load(show: false)
+                if error == nil{
+                    if !self.savePlace(placeMark: placeMarks?.first){
+                       self.setMessage(type: .error("Erro desconhecido."))
+                    }
+                }else{
+                    self.setMessage(type: .error("Não encontramos nenhum local com esse nome."))
+                }
             }
         }
-        
     }
     
     @IBAction func close(_ sender: UIButton) {
         dismiss(animated: true, completion: nil)
     }
-    
-
 }
 
 extension PlaceFinderViewController{
     
     //MARK:Functions
     func load(show:Bool){
-            viewLoading.isHidden = !show
+        viewLoading.isHidden = !show
         if show{
             loading.startAnimating()
         }else{
             loading.stopAnimating()
         }
     }
+    
+    func savePlace(placeMark:CLPlacemark?) -> Bool{
+        guard let placeMark = placeMark , let coordinate = placeMark.location?.coordinate else {
+            setMessage(type: .error("Erro ao localizar"))
+            return false
+        }
+        let name = placeMark.name ?? placeMark.country ?? "Desconhecido"
+        let address = Place.getFormattedAddress(whith: placeMark)
+        print(address)
+        place = Place(name:name,latitude:coordinate.latitude,longitude:coordinate.longitude,address:address)
+        showInMap(place: place)
+        return true
+    }
+    
+    func showInMap(place:Place){
+        
+        let region = MKCoordinateRegion(center: place.coordinate, latitudinalMeters: 3500, longitudinalMeters: 3500)
+        mapView.setRegion(region, animated: true)
+        setMessage(type: .confirmation(place.name))
+    }
+    
+    func setMessage(type:placeFinderMessageType){
+        let title:String, message:String
+        var hasConfirmation:Bool = false
+        
+        switch type {
+            
+        case .confirmation(let name):
+            title = "Local encontrado"
+            message = "Deseja adcionar \(name)?"
+            hasConfirmation = true
+        case .error(let errorMessage):
+            title = "Erro"
+            message = errorMessage
+            hasConfirmation = false
+        }
+        Utils.showAlert(title: title, message: message, confirmation: hasConfirmation, vc: self)
+    }
+   
 }
